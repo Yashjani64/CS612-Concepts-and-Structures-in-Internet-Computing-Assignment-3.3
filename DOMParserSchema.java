604551c6b2ee6c2633bbe1d15117a80b602089e1
Yashjani64 <99891723+Yashjani64@users.noreply.github.com>
2022-11-12 15:15:57 -0500

    Add files via upload

---
+++ DOMParserSchema.java
@@ -0,0 +1,55 @@
+package src;
+import java.io.File;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import org.w3c.dom.Document;
+
+public class DOMParserSchema {
+ public static void main(String[] args) {
+  String schemaName = "djmusicplaylist.xsd";
+  String xmlName = "djmusicplaylist-xsd.xml";
+  Schema schema = loadSchema(schemaName);
+  Document document = parseXmlDom(xmlName);
+  if(document!=null)
+ validateXml(schema, document);
+ }
+ public static void validateXml(Schema schema, Document document) {
+  try {
+   Validator validator = schema.newValidator();
+   System.out.println("Validator Class: " + validator.getClass().getName()); 
+   System.out.println("XML Validation Passed.");
+  } catch (Exception e) {
+   System.out.println(e.toString());
+   System.out.println(" XML Validation Failed.");
+  }
+ }
+ public static Schema loadSchema(String schemaFileName) {
+  Schema schema = null;
+  try {
+   String language = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+   SchemaFactory factory = SchemaFactory.newInstance(language);
+   schema = factory.newSchema(new File(schemaFileName));
+  } catch (Exception e) {
+   System.out.println(e.toString());
+  }
+  return schema;
+ }
+ public static Document parseXmlDom(String xmlName) {
+  Document document = null;
+  try {
+   DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+   DocumentBuilder builder = factory.newDocumentBuilder();
+   document = builder.parse(new File(xmlName));
+   System.out.println("XML Parsing Successful!!");
+  } catch (Exception e) {
+   System.out.println(e.toString());
+   System.out.println("XML Parsing Failed");
+  }
+  return document;
+ }
+}
